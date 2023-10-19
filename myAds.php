1b4df275cacd8d2037bc7bd0fd63dc0c1f0116ef
Yago Alves <yago.melo@souunit.com.br>
2023-10-19 08:02:20 -0300

    Add files via upload

--- myAds.php
+++ myAds.php
@@ -1,37 +1,20 @@
 <?php
 
-include_once "header.php";
-include_once "includes/protect.inc.php";
+    include_once "header.php";
+    include_once "includes/protect.inc.php";
 
-include './classes/Dbh.classes.php';
-include './classes/myAdsInfo.classes.php';
-include './views/myAdsInfo_view.classes.php';
+    include './classes/Dbh.classes.php';
+    include './classes/myAdsInfo.classes.php';
+    include './views/myAdsInfo_view.classes.php';
 
 
-$adsInfo = new MyAdsInfoView();
+    $adsInfo = new MyAdsInfoView();
 ?>
 
-<body>
-    <main>
-        <div class="myAds">
-            <div class="adTitle">
-                <h2>Meus anúncios</h2>
-            </div>
-            <ul>
-                <li>
-                    <a href="adSolo.php">
-                        <h4><?= $adsInfo->FetchAllAdsTitle($_SESSION["userid"]); ?></h4>
-                    </a>
-                        <p><?= $adsInfo->FetchDescription($_SESSION['userid']) ?></p>
-
-                </li>
-            </ul>
-        </div>
-    </main>
-
-</body>
-
+<p><a href="adSolo.php">
+    <?= $adsInfo->FetchAllAdsTitle($_SESSION["userid"]); ?>
+</a></p>
 
 <?php
-include_once "adFooter.php";
+    include_once "adFooter.php";
 ?>
