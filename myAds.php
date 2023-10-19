b07c03e7b2ecf8e892f16841246a7650f3ec02a4
kauamalves <kauademeloalves.kk@gmail.com>
2023-10-19 07:54:24 -0300

    Adicionado CSS novo e descrição no myads

--- myAds.php
+++ myAds.php
@@ -1,20 +1,37 @@
 <?php
 
-    include_once "header.php";
-    include_once "includes/protect.inc.php";
+include_once "header.php";
+include_once "includes/protect.inc.php";
 
-    include './classes/Dbh.classes.php';
-    include './classes/myAdsInfo.classes.php';
-    include './views/myAdsInfo_view.classes.php';
+include './classes/Dbh.classes.php';
+include './classes/myAdsInfo.classes.php';
+include './views/myAdsInfo_view.classes.php';
 
 
-    $adsInfo = new MyAdsInfoView();
+$adsInfo = new MyAdsInfoView();
 ?>
 
-<p><a href="adSolo.php">
-    <?= $adsInfo->FetchAllAdsTitle($_SESSION["userid"]); ?>
-</a></p>
+<body>
+    <main>
+        <div class="myAds">
+            <div class="adTitle">
+                <h2>Meus anúncios</h2>
+            </div>
+            <ul>
+                <li>
+                    <a href="adSolo.php">
+                        <h4><?= $adsInfo->FetchAllAdsTitle($_SESSION["userid"]); ?></h4>
+                    </a>
+                        <p><?= $adsInfo->FetchDescription($_SESSION['userid']) ?></p>
+
+                </li>
+            </ul>
+        </div>
+    </main>
+
+</body>
+
 
 <?php
-    include_once "adFooter.php";
+include_once "adFooter.php";
 ?>
